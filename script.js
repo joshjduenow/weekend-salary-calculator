01228b62e262905f43b2164526307436ac079c74
Joshua Duenow <joshjduenow@gmail.com>
2023-10-27 16:21:42 -0500

    js work on getting submit button to function

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 onReady();
+
+
+
+function handleSubmit(event) {
+    console.log(event);
+  
+    event.preventDefault();
+    let first = document.getElementById('firstName');
+    let last = document.getElementById('lastNmae');
+  
+    console.log(author, affirmation);
+  
+    let tables = document.getElementById('tableBody');
+    tables.innerHTML += 
+    `<tr>
+    <td>${affirmation.value}</td>
+    <td>${author.value}</td>
+    <td><button onclick="deleteText(event)">❌</button></td>
+     </tr>`
+     affirmation.value = ""
+     author.value = ""
+    
+  }
+
+
+
+
+  function onReady() {
+    console.log('Javascript is working!');
+  }
