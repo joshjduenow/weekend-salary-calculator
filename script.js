92c14438f8f3ac42314c3a1932fe03258d090134
Joshua Duenow <joshjduenow@gmail.com>
2023-10-27 16:43:25 -0500

    js and html update

--- script.js
+++ script.js
@@ -7,21 +7,35 @@ function handleSubmit(event) {
   
     event.preventDefault();
     let first = document.getElementById('firstName');
-    let last = document.getElementById('lastNmae');
+    let last = document.getElementById('lastName');
+    let id = document.getElementById('empId');
+    let title = document.getElementById('empTitle');
+    let annualsalary = document.getElementById('salary');
   
-    console.log(author, affirmation);
+    console.log(first, last, id, title, annualsalary);
   
     let tables = document.getElementById('tableBody');
     tables.innerHTML += 
     `<tr>
-    <td>${affirmation.value}</td>
-    <td>${author.value}</td>
+    <td>${first.value}</td>
+    <td>${last.value}</td>
+    <td>${id.value}</td>
+    <td>${title.value}</td>
+    <td>${annualsalary.value}</td>
     <td><button onclick="deleteText(event)">❌</button></td>
      </tr>`
-     affirmation.value = ""
-     author.value = ""
+     first.value = ""
+     last.value = ""
+     id.value = ""
+     title.value = ""
+     annualsalary.value = ""
     
   }
+  function deleteText(event) {
+    event.target.parentElement.parentElement.remove();
+  }
+    
+    
 
 
 
