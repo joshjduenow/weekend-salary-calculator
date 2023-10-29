0d1399b8b6afb241f770169ba93cbb676bf3b671
Joshua Duenow <joshjduenow@gmail.com>
2023-10-29 09:11:58 -0500

    fixed my problems with the data fields not clearing out

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 onReady();
 
-let totalMonthly = 0;
+function onReady() {
+    console.log('Javascript is working!');
+}
 
+let totalMonthly = 0;
 
 function handleSubmit(event) {
     console.log("test");
-
     event.preventDefault();
+
     let first = document.getElementById('firstName').value;
     let last = document.getElementById('lastName').value;
     let id = document.getElementById('empId').value;
     let title = document.getElementById('empTitle').value;
     let annualSalary = document.getElementById('salary').value;
-    let budget = document.getElementById('totalBudget').value;
-    
 
-    document.getElementById('tableBody').innerHTML +=
-     `<tr>
+    tableBody.innerHTML +=
+        `<tr>
     <td>${first}</td>
     <td>${last}</td>
     <td>${id}</td>
@@ -24,37 +25,27 @@ function handleSubmit(event) {
     <td>${annualSalary}</td>
     <td><button onclick="deleteText(event)">❌</button></td>
      </tr>`
-    // first.value = ""
-    // last.value = ""
-    // id.value = ""
-    // title.value = ""
-    // annualSalary.value = ""
+    firstName.value = ""
+    lastName.value = ""
+    empId.value = ""
+    empTitle.value = ""
+    salary.value = ""
 
     totalMonthly += Number(annualSalary);
-    document.getElementById('totalBudget').textContent = Number(totalMonthly);
-    if(totalMonthly > 20000){
-        totalBudget.innerHTML = `<p class='inRed'> ${totalMonthly}</p>`}
-         else {totalBudget.innerHTML = `<p>${totalMonthly}</p>`};
+    document.getElementById('totalBudget')
+    if (totalMonthly > 20000) {
+        totalBudget.innerHTML = `<p class='over-budget'> ${totalMonthly}</p>`
+    }
+    else { totalBudget.innerHTML = `<p>${totalMonthly}</p>` };
 
- }
+
+
+
+
+}
 function deleteText(event) {
     event.target.parentElement.parentElement.remove();
 }
 
-//   function addTotalMonthly() {
-//     let budget = document.getElementById('totalBudget');
-//     console.log("Am I balancing a budget",budget);
-
-//     budget.innerHTML +=
-
-//     totalMonthly+=annualSalary;
-//   }
 
 
-
-
-
-
-function onReady() {
-    console.log('Javascript is working!');
-}
